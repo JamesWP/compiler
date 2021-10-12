dcd4c4bd27f7a8fd3a1520404a8b425cc12fbcfb
James Peach <jpeach6@bloomberg.net>
2021-10-12 13:26:34 +0100

    add lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,45 +1,114 @@
+use std::iter::FromIterator;
+
+pub struct Lexer {
+    source: Box<dyn std::io::Read>,
+    filename: String,
+    pos: Pos,
+}
+
+#[derive(Clone, Debug)]
 pub struct Pos {
     line: u32,
     col: u32,
 }
 
+#[derive(Debug)]
 pub struct Location {
     filename: String,
     start: Pos,
     end: Pos,
 }
 
+#[derive(Debug, PartialEq)]
 pub enum Token {
     StringLiteral(String),
+}
+
+impl Lexer {
+    pub fn new_from_string(content: String) -> Lexer {
+        let bytes = std::io::Cursor::new(content.bytes().collect::<Vec<_>>());
+
+        Lexer {
+            source: Box::new(bytes),
+            filename: "raw_input_from_string.txt".to_owned(),
+            pos: Pos::default()
+        }
+    }
 }
 
 impl Pos {
     pub fn new(line: u32, col: u32) -> Pos {
         Pos { line, col }
     }
-}
 
-impl Location {
-    pub fn new(filename: &str, start: Pos, end: Pos) -> Location {
-        Location { filename: filename.to_owned(), start, end }
+    pub fn advance(&mut self) {
+        self.col += 1;
     }
 }
 
-pub type Lex(Location, Token);
+impl Default for Pos {
+    fn default() -> Pos {
+        Pos::new(0, 0)
+    }
+}
+
+impl Location {
+    pub fn new(filename: &str, start: &Pos, end: &Pos) -> Location {
+        Location {
+            filename: filename.to_owned(),
+            start: start.clone(),
+            end: end.clone(),
+        }
+    }
+}
+
+pub type Lex = (Location, Token);
+
+impl Lexer {
+    fn next_character(&mut self) -> Option<char> {
+        let mut buf = [0; 1];
+        let num_bytes = self.source.read(&mut buf).ok()?;
+
+        if num_bytes == 0 {
+            return None
+        }
+
+        self.pos.advance();
+
+        Some(buf[0] as char)
+    }
+}
+
+impl Iterator for Lexer {
+    type Item = Lex;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        let start = self.pos.clone();
+
+        let mut chars = Vec::new();
+
+        chars.push(self.next_character()?);
+        let char2 = self.next_character();
+        if let Some(c) = char2 {
+            chars.push(c);
+        }
+
+        let location = Location::new(&self.filename, &start, &self.pos);
+
+        let literal = String::from_iter(&chars).to_string();
+
+        Some((location, Token::StringLiteral(literal)))
+    }
+}
+
+#[test]
+fn test_lexer() {
+    let mut lexer = Lexer::new_from_string("Hello World".to_owned());
+
+    let tokens: Vec<_> = lexer.collect();
+
+    assert_eq!(tokens.len(), 6);
 
 
-
-pub fn tokenize(input: &str, filename: &str, line: u32) -> Vec<Lex> {
-    let start_col = 0;
-    let end_col = start_col + 10;
-    let mut tokens = Vec::new();
-
-    tokens.push(
-        (
-            Location::new(filename, Pos::new(line, start_col), Pos::new(line, end_col)),
-            Token::StringLiteral("Tooken".to_owned())
-        )
-    );
-
-    tokens
+    assert_eq!(tokens[0].1, Token::StringLiteral("He".to_owned()));
 }
