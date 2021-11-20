534e74a6a706b357d3cf1b9eae886469f9a875af
James Peach <jpeach6@bloomberg.net>
2021-11-20 11:54:43 +0000

    refactored

--- src/platform.rs
+++ src/platform.rs
@@ -1,32 +1,32 @@
-use std::fmt::Display;
 use crate::ast;
+use std::fmt::Display;
 
 #[derive(Clone)]
 pub enum x86_64_reg {
-    RAX, // register a extended
-    RBX, // register b extended
-    RCX, // register c extended
-    RDX, // register d extended
-    RBP, // register base pointer (start of stack)
-    RSP, // register stack pointer (current location in stack, growing downwards)
-    RSI, // register source index (source for data copies)
-    RDI, // register destination index (destination for data copies)
-    R8,  // register 8
-    R9,  // register 9
-    R10, // register 10
-    R11, // register 11
-    R12, // register 12
-    R13, // register 13
-    R14, // register 14
-    R15, // register 15
-    EAX, // lower half of RAX
-    EBX, // lower half of RBX
-    ECX, // lower half of RCX
-    EDX, // lower half of RDX
-    EBP, // lower half of RBP
-    ESP, // lower half of RSP
-    ESI, // lower half of RSI
-    EDI, // lower half of RDI
+    RAX,  // register a extended
+    RBX,  // register b extended
+    RCX,  // register c extended
+    RDX,  // register d extended
+    RBP,  // register base pointer (start of stack)
+    RSP,  // register stack pointer (current location in stack, growing downwards)
+    RSI,  // register source index (source for data copies)
+    RDI,  // register destination index (destination for data copies)
+    R8,   // register 8
+    R9,   // register 9
+    R10,  // register 10
+    R11,  // register 11
+    R12,  // register 12
+    R13,  // register 13
+    R14,  // register 14
+    R15,  // register 15
+    EAX,  // lower half of RAX
+    EBX,  // lower half of RBX
+    ECX,  // lower half of RCX
+    EDX,  // lower half of RDX
+    EBP,  // lower half of RBP
+    ESP,  // lower half of RSP
+    ESI,  // lower half of RSI
+    EDI,  // lower half of RDI
     R8D,  // lower half of R8
     R9D,  // lower hald of R9
     R10D, // lower hald of R10
@@ -35,6 +35,53 @@ pub enum x86_64_reg {
     R13D, // lower hald of R13
     R14D, // lower hald of R14
     R15D, // lower hald of R15
+}
+
+pub struct StackRelativeLocation {
+    // offset from this register (RBP)
+    reg: x86_64_reg,
+    // location relative to register
+    offset: i32,
+    // size of location,
+    size: usize,
+}
+
+pub type StackLayout = Vec<ParameterInfo>;
+
+pub struct ParameterInfo {
+    pub name: String,
+    pub param_type: ast::TypeDefinition,
+    pub reg: Option<x86_64_reg>,
+    pub stack_allocation: StackRelativeLocation,
+}
+
+pub struct DecimalLiteral {
+    value: i32
+}
+
+const INTEGER_64_REGISTER_ORDER: [x86_64_reg; 6] = [
+    x86_64_reg::RDI,
+    x86_64_reg::RSI,
+    x86_64_reg::RDX,
+    x86_64_reg::RCX,
+    x86_64_reg::R8,
+    x86_64_reg::R9,
+];
+const INTEGER_32_REGISTER_ORDER: [x86_64_reg; 6] = [
+    x86_64_reg::EDI,
+    x86_64_reg::ESI,
+    x86_64_reg::EDX,
+    x86_64_reg::ECX,
+    x86_64_reg::R8D,
+    x86_64_reg::R9D,
+];
+
+pub struct ParameterPlacement {
+    num_integer_args: usize,
+}
+
+pub struct Parameter {
+    pub reg: Option<x86_64_reg>,
 }
 
 impl Display for x86_64_reg {
@@ -70,61 +117,30 @@ impl Display for x86_64_reg {
     }
 }
 
-pub struct StackRelativeLocation {
-    // offset from this register (RBP)
-    reg: x86_64_reg,
-    // location relative to register
-    offset: i32,
-    // size of location,
-    size: usize,
+impl Display for DecimalLiteral {
+    fn fmt(&self, formatter: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+        write!(formatter, "${}", self.value)
+    }
 }
 
-pub type StackLayout = Vec<ParameterInfo>;
-
-pub struct ParameterInfo {
-    pub name: String,  
-    pub param_type: ast::TypeDefinition,
-    pub reg: Option<x86_64_reg>,
-    pub stack_allocation: StackRelativeLocation,
-}
-
-const INTEGER_64_REGISTER_ORDER : [x86_64_reg; 6]= [
-    x86_64_reg::RDI, 
-    x86_64_reg::RSI, 
-    x86_64_reg::RDX, 
-    x86_64_reg::RCX, 
-    x86_64_reg::R8, 
-    x86_64_reg::R9, 
-];
-const INTEGER_32_REGISTER_ORDER : [x86_64_reg; 6]= [
-    x86_64_reg::EDI, 
-    x86_64_reg::ESI, 
-    x86_64_reg::EDX, 
-    x86_64_reg::ECX, 
-    x86_64_reg::R8D, 
-    x86_64_reg::R9D, 
-];
-
-pub struct ParameterPlacement {
-    num_integer_args: usize
-}
-
-pub struct Parameter {
-    pub reg: Option<x86_64_reg>
+impl DecimalLiteral {
+    pub fn new<T: std::borrow::Borrow<i32>>(value: T) -> DecimalLiteral {
+        DecimalLiteral {
+            value: *value.borrow()
+        }
+    }
 }
 
 impl Parameter {
-    pub fn new(reg: x86_64_reg) -> Parameter{
-        Parameter {
-            reg: Some(reg)
-        }
+    pub fn new(reg: x86_64_reg) -> Parameter {
+        Parameter { reg: Some(reg) }
     }
 }
 
 impl Default for ParameterPlacement {
     fn default() -> ParameterPlacement {
         ParameterPlacement {
-            num_integer_args: 0
+            num_integer_args: 0,
         }
     }
 }
@@ -132,12 +148,16 @@ impl Default for ParameterPlacement {
 impl ParameterPlacement {
     pub fn place(&mut self, param_type: &ast::TypeDefinition) -> Parameter {
         match param_type {
-            ast::TypeDefinition{base_type: ast::BaseType::INT} => {
+            ast::TypeDefinition {
+                base_type: ast::BaseType::INT,
+            } => {
                 let reg = INTEGER_32_REGISTER_ORDER[self.num_integer_args].clone();
                 self.num_integer_args += 1;
                 Parameter::new(reg)
-            },
-            _ => { unimplemented!(); }
+            }
+            _ => {
+                unimplemented!();
+            }
         }
     }
 }
@@ -165,12 +185,17 @@ impl Display for StackRelativeLocation {
 }
 
 impl ParameterInfo {
-    pub fn new(name: &str, reg: x86_64_reg, param_type: &ast::TypeDefinition, stack_allocation: StackRelativeLocation) -> ParameterInfo {
+    pub fn new(
+        name: &str,
+        reg: x86_64_reg,
+        param_type: &ast::TypeDefinition,
+        stack_allocation: StackRelativeLocation,
+    ) -> ParameterInfo {
         ParameterInfo {
             name: name.to_owned(),
             reg: Some(reg),
             param_type: param_type.clone(),
-            stack_allocation
+            stack_allocation,
         }
     }
 }
