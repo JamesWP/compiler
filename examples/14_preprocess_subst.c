2394846d2de6a0ad7656356fdb56da1c1292d5f9
James Peach <jpeach6@bloomberg.net>
2022-03-20 10:58:48 +0000

    add simple preprocess example

---
+++ examples/14_preprocess_subst.c
@@ -0,0 +1,22 @@
+# define find replace
+# define a b
+# define true false
+
+int main(int argc, const char* argv[]) {
+  int replace = 0;
+
+  find += 1;
+
+  int false = 0;
+
+  if (replace == true){
+    return 1;
+  }
+  int a = 123;
+
+  if (b != a) {
+    return 2;
+  }
+
+  return 0;
+}
