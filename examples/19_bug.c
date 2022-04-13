4ee77f299dd90c22bf0ef1b4fbb87af4c1733542
James Peach <james_peach01@hotmail.co.uk>
2022-04-13 21:41:26 +0100

    add bug example

---
+++ examples/19_bug.c
@@ -0,0 +1,11 @@
+int mux(int a0, int a1, int a2, int a3) {
+    return a2;
+}
+int main(int argc, const char* argv[]) {
+    int a = mux(1,2,3,0*0);
+    if (a != 3) return 1;
+    int b = mux(1,2,3,mux(0,0,0,0));
+    if (b != 3) return 1;
+
+    return 0;
+}
